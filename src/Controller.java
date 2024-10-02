70d8b042ef03834cc03c60fda5e533a8b424ae69
srhrsb <sebastian.boettcher2@srh.de>
2024-10-02 14:05:03 +0200

    Aufgabe 2 Mitarbeiter hinzufügen

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,13 @@
+import java.time.LocalDate;
+
 public class Controller {
 
     private EmployeeDAO employeeDB;
 
     public Controller(EmployeeDAO employeeDB) {
         this.employeeDB = employeeDB;
+
+        employeeDB.addEmployee("h", "t","1","Test",12345687, 100, LocalDate.of(2024, 12, 3));
 
     }
 
